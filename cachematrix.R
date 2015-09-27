3ed6926b378cea105b90aab00fbbaebc8624a34a
Uma Kukkala <kumreddy91@gmail.com>
2015-09-27 02:40:36 -0700

    comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
-#makeCacheMatrix returns a list to set, get, setinverse and getinverse of a given matrix
-#for example, makeCacheMatrix$setinverse sets inverse value to inv(cache)
 
-## Write a short comment describing this function
+#makeCacheMatrix(marix1) returns a list to set, get, setinverse and getinverse of a given matrix
+#for example, makeCacheMatrix$setinverse sets inverse value to inv, a cache variable.
+#cacheSove(m) returns inverse of matrix m, returns either cache value(if not NULL) or computed value
+
+#
+
+## This function returns a list,
+#1. setting matrix 2.getting matrix 3. setting inverse into cache
+#4.getting cached inverse
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL #Initialized to Null
@@ -22,7 +26,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 #This function checks for inverse in cache. If cache value "inv" is NULL then
-#it computes inverse. Otherwise returns cached value.
+#it computes and returs inverse. Otherwise returns cached value.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -33,11 +37,11 @@ cacheSolve <- function(x, ...) {
     }
     data <- x$get()
     inv <- solve(data, ...)
-    x$setinverse(inv) #sending(or)setting computer inv to cache
+    x$setinverse(inv) #sending(or)setting computed inv to cache
     inv
 }
 
 #Desclaimer: I just modified prof. Peng's code of 'vector caching'.
-#I replaced mean,m and mean() with inverse, inv and solve(). Thanks to previous students
+#I replaced mean,m and mean() with inverse, inv and solve(). Thanks to previous students.
 #This assignment is very informative in learning about caching.
 
